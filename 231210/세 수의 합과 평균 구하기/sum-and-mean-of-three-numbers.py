ee6a2ebf7f3014e6fc2c8d27952d40abd71af541
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-10 20:13:26 +0900

    Title: 세 수의 합과 평균 구하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231210/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
+++ 231210/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -1,3 +1,5 @@
-data = input().split()
-print(sum(data))
-print(sum(data) / len(data))
+data = list(input().split())
+a, b, c = int(data[0]), int(data[1]), int(data[2])
+p = a + b + c
+print(p)
+print(round(p / 3))
