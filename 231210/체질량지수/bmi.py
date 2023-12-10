1245b778b6da54e3a3dab7d89e39dd64675d8998
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-10 20:31:41 +0900

    Title: 체질량지수, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 231210/체질량지수/bmi.py
+++ 231210/체질량지수/bmi.py
@@ -1,6 +1,6 @@
 data = input().split()
-h, m = int(data[0])*100, int(data[1])
-bmi = int(m / h**2)
+h, m = int(data[0]), int(data[1])
+bmi = int(m*10000 / h**2)
 print(bmi)
 if bmi >= 25:
     print("Obesity")
