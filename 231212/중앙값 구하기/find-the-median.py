9843c710c8a045f97c66332918fb6e08bdc462ed
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-12 12:44:36 +0900

    Title: 중앙값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231212/중앙값 구하기/find-the-median.py
@@ -0,0 +1,20 @@
+data = input().split()
+a, b, c = int(data[0]), int(data[1]), int(data[2])
+if a > b:
+    if a > c:
+        if b > c:
+            ans = b
+        else:
+            ans = c
+    else:
+        ans = a
+else:
+    if b > c:
+        if a > c:
+            ans = a
+        else:
+            ans = c
+    else:
+        ans = b
+
+print(l[1])
