e95a2a33a4f358955abf745f72e8b14fcda0cecb
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-12 10:25:54 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231212/두 사람/two-person.py
+++ 231212/두 사람/two-person.py
@@ -2,7 +2,7 @@ data = input().split()
 aa, ag = int(data[0]), int(data[1])
 data = input().split()
 ba, bg = int(data[0]), int(data[1])
-if (aa >= 19 and ag = 'M') or (ba >= 19 and bg == 'M'):
+if (aa >= 19 and ag == 'M') or (ba >= 19 and bg == 'M'):
     print(1)
 else:
     print(0)
