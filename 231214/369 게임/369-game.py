6b6554eb861b2f794dbd3558a72997bd87f0b371
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-14 15:52:16 +0900

    Title: 369 게임, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 231214/369 게임/369-game.py
+++ 231214/369 게임/369-game.py
@@ -2,7 +2,7 @@ n = int(input())
 for i in range(1, n+1):
     ans = i
     for j in str(i):
-        if (int(j) % 3) == 0:
+        if (int(j) % 3) == 0 and int(j) != 0:
             ans = 0
         elif (i % 3) == 0:
             ans = 0
