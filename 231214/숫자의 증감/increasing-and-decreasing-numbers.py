9fd1d5b5aa74a93a928bc3d79371ed987771bd67
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-14 14:52:22 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231214/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,8 @@
+data = input().split()
+c, n = str(data[0]), int(data[1])
+if c == 'A':
+    for i in range(1, n+1):
+        print(i, end=' ')
+else:
+    for i in rnage(n, 0, -1):
+        print(i, end=' ')
