e391725fccf263e92bdcd5717c9f797e17e8ed1e
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-14 15:47:36 +0900

    Title: 369 게임, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/369 게임/369-game.py
+++ 231214/369 게임/369-game.py
@@ -1,8 +1,10 @@
 n = int(input())
 for i in range(1, n+1):
-    data = str(i).split('')
-    for j in data:
-        if (j % 3) == 0 or (i % 3) == 0:
-            print(0, end=' ')
+    for j in str(i):
+        if (int(j) % 3) == 0:
+            ans = 0
+        elif (i % 3) == 0:
+            ans = 0
         else:
-            print(i, end=' ')
+            ans = i
+    print(ans, end=' ')
