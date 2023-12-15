5e3d5baf8723456ff658905ef18577d64569f3d3
kjew3174 <153427611+kjew3174@users.noreply.github.com>
2023-12-15 09:28:41 +0900

    Title: 청소당번 정하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231215/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,14 @@
+n = int(input())
+c = 0; h = 0; t = 0;
+for date in range(n):
+    if date == 0:
+        pass
+    elif (date % 12) == 0:
+        t += 1
+    elif (date % 3) == 0:
+        h += 1
+    elif (date % 2) == 0:
+        c += 1
+    else:
+        pass
+print(c, h, t)
